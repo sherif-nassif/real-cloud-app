6e67040a19c7a1a6cf3d06bd0b42c2a03cff50ed
sherif-nassif <sherif.nassif@gmail.com>
2026-05-06 22:42:48 +0000

    7th commit

--- terraform/Compute.tf
+++ terraform/Compute.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_compute_instance" "web_server" {
   name         = "app-server"
-  machine_type = "e2-highmem-2"
+  machine_type = "e2-highmem-4"
   zone         = "europe-north2-a"
 
   boot_disk {
@@ -36,5 +36,5 @@ resource "google_compute_instance" "web_server" {
 }
 
 output "instance_ip" {
-  value = google_compute_instance.web_server.network_interface[0].access_config[0].nat_ip
+  value = google_compute_instance.web_server.network_interface[0].network_ip
 }
